ffdb0181547040530d0078deb2ed51f1a81dc8cd
Ayush <65536670+ayushwagdre@users.noreply.github.com>
2020-12-16 14:13:59 +0530

    Add files via upload

---
+++ Media Player/mediaPlayer.py
@@ -0,0 +1,78 @@
+from tkinter import *
+import pygame
+from doublyLinkedList import *
+
+dlist = Doublylinkedlist()
+root = Tk()
+pygame.mixer.init()
+
+def playMusic():
+    #play.destroy()
+    pygame.mixer.music.load(dlist.head.data)
+    pygame.mixer.music.play()
+
+
+def stopMusic():
+    pygame.mixer.music.stop()
+
+
+def pauseMusic():
+    pygame.mixer.music.pause()
+
+
+def unpauseMusic():
+    pygame.mixer.music.unpause()
+
+
+def prevMusic():
+    pygame.mixer.music.load(dlist.getPrev())
+    pygame.mixer.music.play()
+
+
+def nextMusic():
+    pygame.mixer.music.load(dlist.getNext())
+    pygame.mixer.music.play()
+
+
+def loadMediaPlayer(dirPath):
+    listFiles = dlist.getFileName(dirPath)
+    for i in listFiles:
+        dlist.insert(i)
+
+
+def mediaPlayerCanvas():
+    root.title("SV Music")
+    canvas = Canvas(root,width = 300,height = 300)
+    canvas.pack()
+    canvas.configure(background='black')
+    root.configure(background='black')
+
+    stopPhoto = (PhotoImage(file="Images/stop.png"))
+    playPhoto = PhotoImage(file="Images/play.png")
+    pausePhoto = PhotoImage(file="Images/pause.png")
+    playAgainPhoto = PhotoImage(file="Images/playagain.png")
+    nextPhoto = PhotoImage(file="Images/next.png")
+    prevPhoto = PhotoImage(file="Images/prev.png")
+
+    play = Button(root,image = playPhoto,command = playMusic).pack(side = LEFT)
+    stop = Button(root,image = stopPhoto,command = stopMusic).pack(side = LEFT)
+    pause = Button(root,image = pausePhoto,command = pauseMusic).pack(side = LEFT)
+    unPause = Button(root,image = playAgainPhoto,command = unpauseMusic).pack(side = LEFT)
+    next = Button(root,image = nextPhoto,command = nextMusic).pack(side = LEFT)
+    prev = Button(root,image = prevPhoto,command = prevMusic).pack(side = LEFT)
+
+    prev.pack(side = LEFT)
+    play.pack(side = LEFT)
+    pause.pack(side = LEFT)
+    stop.pack(side = LEFT)
+    next.pack(side = LEFT)
+    unPause.pack(side = RIGHT)
+
+if __name__ == '__main__':
+    loadMediaPlayer("song")
+    dlist.display()
+    mediaPlayerCanvas()
+    root.mainloop()
+
+#button_frame.columnconfigure(0,weight=1)
+#button_frame.columnconfigure(1,weight=1)
